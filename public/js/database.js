abe82ad8cbc6420a49738c902acf89956ce31cf1
mheilner <mark.heilner4@gmail.com>
2023-04-03 22:52:46 -0600

    working on the blog post editor

---
+++ public/js/database.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
+const userName = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAME;
+
+if (!userName) {
+  throw Error('Database not configured. Set environment variables');
+}
+
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+
+// const client = new MongoClient(url);
+// const userCollection = client.db('simon').collection('user');
+// const scoreCollection = client.db('simon').collection('score');
+
