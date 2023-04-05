7e050a02ac3010557c6dfc53610792a4bf398a31
mheilner <mark.heilner4@gmail.com>
2023-04-05 13:24:28 -0600

    everything but websocket as checked by austin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { PeerProxy } = require('./peerProxy.js');
 const authCookieName = 'token';
 
 // The service port may be set on the command line
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
