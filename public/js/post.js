fb5942d8f434d47ccadae821ed7a6cb1a14c0a83
mheilner <mark.heilner4@gmail.com>
2023-04-05 14:08:10 -0600

    Final commit - websocket doesnt work

--- public/js/post.js
+++ public/js/post.js
@@ -77,4 +77,77 @@ async function loadArticles() {
   }
   
   loadArticles();
+
+
+  // class Game {
+  //   socket;
+  
+  //   constructor() {
+  
+  //     const playerNameEl = document.querySelector('.player-name');
+  //     playerNameEl.textContent = this.getPlayerName();
+  
+  //     this.configureWebSocket();
+  //   }
+
+  
+  //   getPlayerName() {
+  //     return localStorage.getItem('userName') ?? 'Mystery player';
+  //   }
+  //   // Functionality for peer communication using WebSocket
+  //   configureWebSocket() {
+  //     const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+  //     this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+  //     this.socket.onopen = (event) => {
+  //       this.displayMsg('system', 'game', 'connected');
+  //     };
+  //     this.socket.onclose = (event) => {
+  //       this.displayMsg('system', 'game', 'disconnected');
+  //     };
+  //     this.socket.onmessage = async (event) => {
+  //       const msg = JSON.parse(await event.data.text());
+  //       if (msg.type === GameEndEvent) {
+  //         this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
+  //       } else if (msg.type === GameStartEvent) {
+  //         this.displayMsg('player', msg.from, `started a new game`);
+  //       }
+  //     };
+  //   }
+  
+    // displayMsg(cls, from, msg) {
+    //   const chatText = document.querySelector('#player-messages');
+    //   chatText.innerHTML =
+    //     `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
+    // }
+  
+  //   broadcastEvent(from, type, value) {
+  //     const event = {
+  //       from: from,
+  //       type: type,
+  //       value: value,
+  //     };
+  //     this.socket.send(JSON.stringify(event));
+  //   }
+  // }
+  
+  // const game = new Game();
+  
+  
+  // const socket = new WebSocket('ws://localhost:4000');
+
+  // socket.addEventListener('open', (event) => {
+  //   console.log('Connected to server');
+  // });
+  
+  // socket.addEventListener('message', (event) => {
+  //   console.log(`Received message: ${event.data}`);
+  // });
+  
+  // socket.addEventListener('close', (event) => {
+  //   console.log('Disconnected from server');
+  // });
+  
+  // function sendMessage(message) {
+  //   socket.send(message);
+  // }
   
